521c74b77f1f6e1afee359b58215b2156471ea5d
WTobor <weronika.tobor@op.pl>
2017-02-26 12:03:20 +0100

    Add project files.

---
+++ BoardGameNook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoardGameNook.Startup))]
+namespace BoardGameNook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
