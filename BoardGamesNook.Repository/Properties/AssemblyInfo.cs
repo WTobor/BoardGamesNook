4b5cec91a70479fe7a416fa5706dab5006c4177b
WTobor <weronika.tobor@op.pl>
2017-03-05 16:06:06 +0100

    fix in project names

--- BoardGamesNook.Repository/Properties/AssemblyInfo.cs
+++ BoardGamesNook.Repository/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BoardGameNook.Repository")]
+[assembly: AssemblyTitle("BoardGamesNook.Repository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BoardGameNook.Repository")]
+[assembly: AssemblyProduct("BoardGamesNook.Repository")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- BoardGamesNook/Global.asax
+++ BoardGamesNook/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoardGameNook.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoardGamesNook.MvcApplication" Language="C#" %>
