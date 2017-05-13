49c0d50e254832d391aacf7ff7696a21d13fc861
WTobor <weronika.tobor@op.pl>
2017-05-13 15:08:53 +0200

    added model for GamerTable and GameParticipation

---
+++ BoardGamesNook.Model/GameParticipation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BoardGamesNook.Model
+{
+    public class GameParticipation
+    {
+        public int Id { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public int CreatedUserId { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; }
+        public int? ModifiedUserId { get; set; }
+        public int GameTableId { get; set; }
+        public GameTable GameTable { get; set; }
+        public int GamerId { get; set; }
+        public Gamer Gamer { get; set; }
+        public bool IsConfirmed { get; set; }
+        public int Status { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ BoardGamesNook.Model/GameTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoardGamesNook.Model
+{
+    public class GameTable
+    {
+        public int Id { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public int CreatedUserId { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; }
+        public int? ModifiedUserId { get; set; }
+        public int PlayersNumber { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsFull { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public bool Active { get; set; }
+        public List<GameParticipation> GameParticipationInfo { get; set; }
+    }
+}
