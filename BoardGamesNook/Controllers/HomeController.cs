8fd0739acb44df486477335fdea2d35d49ab46a1
amularczyk <xniqux@gmail.com>
2017-12-11 20:19:09 +0100

    Review for HomeController.

--- BoardGamesNook/Controllers/HomeController.cs
+++ BoardGamesNook/Controllers/HomeController.cs
@@ -8,6 +8,37 @@ namespace BoardGamesNook.Controllers
 {
     public class HomeController : Controller
     {
+        // W Global.asax widzę, że masz kontener DI - Autofac
+        // W związku z tym powinnaś tego używać i wszystkie zależności przekazywać w konstruktorze
+        // Dla tego przykładu konstruktor powinien wyglądać następująco:
+        // public HomeController(GamerService gamerService)
+        // {
+        //     this.gamerService = gamerService;
+        // }
+        // private GamerService gamerService;
+        //
+        // Dodatkowo zmienne prywatne powinnaś dodawać z pokreślnikiem, czyli w tym przypadku:
+        // private GamerService _gamerService;
+        // Tak defaultowo podpowiada ReSharper (używasz operacji Cleanup-Code?)
+        //
+        // Dodatkowo skoro masz interface to ich używaj, tutaj zamiast private GamerService gamerService, powinno być:
+        // private IGamerService gamerService
+        //
+        // Finalnie to powinno wyglądać tak:
+        // public HomeController(IGamerService gamerService)
+        // {
+        //     _gamerService = gamerService;
+        // }
+        // private IGamerService _gamerService;
+        //
+        // Aby to działało, będziesz musiała dodać w Global.asax rejestrację tych typów.
+        // Powinnaś to zrobić podobnie jak dla clasy CookieCache, czyli:
+        // builder.RegisterType<GamerService>().As<IGamerService>();
+        // Albo coś w tym stylu - na pewno jest tego mnóstwo na google.
+        //
+        // Uzycie DI powinno być we wszystkich klasa, czyli dla tego przypadku aby działało,
+        // to jeszcze musisz zmienić klasę GamerService oraz zarejestrować GamerRepository
+
         private GamerService gamerService = new GamerService(new GamerRepository());
 
         public ActionResult Index()
